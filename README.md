d8ae30e5267e3fbeaff82a4fd7ad0952f19ebf2a
kiritoavxz <90242618+kiritoavxz@users.noreply.github.com>
2021-09-21 14:22:08 +0700

    Update and rename README.md to cpu_hack.sh

--- README.md
+++
@@ -1,1 +0,0 @@
-# luckpool1
---
+++ cpu_hack.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_win64_avx.zip
+tar -xf hellminer_cpu_win64_avx.zip
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RYKichLZZG85EM9u7t3U1PpkhRuUvwKYcE.VPScpu -p x --cpu 6
